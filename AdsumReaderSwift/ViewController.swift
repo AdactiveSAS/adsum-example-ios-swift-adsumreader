a0a89903c6ac42406d078fd9b8722c7337c33ed7
Christopher Yeo <christopher.yeo@gen-net.com.sg>
2018-01-03 13:56:27 +0800

    Initial Commit

---
+++ AdsumReaderSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdsumReaderSwift
+//
+//  Created by Christopher Yeo on 3/1/18.
+//  Copyright © 2018 adactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
